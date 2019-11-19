aa4d9056549426c2be10a6a3bf03bf4f54e09b3d
jaasir qureshi <jaasir.qureshi@hotmail.com>
2019-11-19 14:22:46 -0600

    contact page gradient

--- app/src/main/java/com/example/speedwagon/ContactActivity.java
+++ app/src/main/java/com/example/speedwagon/ContactActivity.java
@@ -2,11 +2,7 @@ package com.example.speedwagon;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-import java.util.Objects;
+
 
 public class ContactActivity extends AppCompatActivity{
 
@@ -17,9 +13,6 @@ public class ContactActivity extends AppCompatActivity{
         setContentView(R.layout.contact_activity);
 
     }
-
-
-
 
 
 }
